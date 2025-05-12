bdf166e08f36ab6c5ed8f1a30cb96e6db4c0afdb
andyalv <andy.red02@gmail.com>
2025-05-11 18:01:50 -0600

    Modified for typescript

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -23,6 +23,9 @@ return {
 						"rafamadriz/friendly-snippets",
 						config = function()
 							require("luasnip.loaders.from_vscode").lazy_load()
+
+							require("luasnip").filetype_extend("typescript", { "html", "javascript" })
+							require("luasnip").filetype_extend("typescriptreact", { "html", "javascript" })
 						end,
 					},
 				},
